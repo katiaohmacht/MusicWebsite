e55a5a41a80b1988825b0c93e3dbbad98ff59ba2
Katia Ohmacht <katia.ohmacht@icloud.com>
2022-04-17 22:33:31 -0400

    change quotes

--- pull.php
+++ pull.php
@@ -3,7 +3,7 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-shell_exec( ‘sudo -u katia /home/katia/git-pull-web.sh’ );
+shell_exec( "sudo -u katia /home/katia/git-pull-web.sh" );
 }
 
 ?>hi
