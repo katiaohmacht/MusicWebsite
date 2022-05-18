f2cb9b74ec9185f4ea3abaf19957d8582fba2379
katiaohmacht <99374417+katiaohmacht@users.noreply.github.com>
2022-05-18 15:19:37 -0400

    .

--- MMfunctions.js
+++ MMfunctions.js
@@ -1,19 +1,23 @@
 function passWord() {
-var testV = 1;
-var pass1 = prompt('Please Enter Your Password',' ');
-while (testV < 3) {
-if (!pass1)
-history.go(-1);
-if (pass1.toLowerCase() == "password") {
-window.open('https://musicmeasures.ddns.net/instructoraccess.html');
-break;
-}
-testV+=1;
-var pass1 =
-prompt('Password Incorrect, Please Try Again.','Password');
-}
-if (pass1.toLowerCase()!="password" & testV ==3)
-window.open('https://musicmeasures.ddns.net/index.html');
-history.go(-1);
-return " ";
+    let testV = 1;
+    var pass1 = prompt('Please Enter Your Password', ' ');
+    if (pass1 !== null) {
+        while (testV < 3) {
+            if (!pass1) history.go(-1);
+            if (pass1.toLowerCase() == "password") {
+                console.log("it worked!!");
+                window.open('https://musicmeasures.ddns.net/instructoraccess.html');
+                break;
+            }
+            testV += 1;
+            let pass1 =
+            prompt('Password Incorrect, Please Try Again.','Password');
+        }
+        if (pass1.toLowerCase()!="password" & testV == 3) {
+            window.open('https://musicmeasures.ddns.net/index.html');
+        } else {
+            history.go(-1);
+        }
+        return " ";
+    }
 }
